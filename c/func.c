865e6c93730362de0fe195b9f6025000b2c57b12
Uzair Shahed Islam <uzair.shahed95@gmail.com>
2025-05-23 17:29:02 -0400

    Added sources

--- c/func.c
+++ c/func.c
@@ -1,3 +1,7 @@
+// Code based on tutorial by Bro Code
+// Playlist: https://www.youtube.com/watch?v=2ciUcosJFBc&list=PLZPZq0r_RZOOzY_vR4zJM32SqsSInGMwe
+// May be modified or re-typed by me for learning purposes
+
 #include <stdio.h>
 #include <string.h>
 
